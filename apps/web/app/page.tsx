91f45093cddcc26290616426f41c86446356df48
moknine elhadj <mokninemoknine999@gmail.com>
2026-08-31 13:53:21 +0100

    configured the Next.js design system by adding shadcn and Installing new Expo project for the mobile platform

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,9 @@
 import { Button } from '@/components/ui/button';
-import type { IELTSModule } from '@ielts/types';
-
-const moduleName: IELTSModule = 'speaking';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold text-red-500">IELTS Platform</h1>
-      <p>Current module: {moduleName}</p>
-      <Button variant="default" size="lg">
-        Start {moduleName} module
-      </Button>
-    </main>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Button variant={'destructive'}>Click me</Button>
+    </div>
   );
 }
